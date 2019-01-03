e9337e9916bb6b8981ca4adb038341050b6ccefa
gellmr <gellmr@gmail.com>
2019-01-03 09:35:39 +0800

    Store page can now add products to cart. Http PUT verb enabled in web.config

---
+++ IdentityApplication/Controllers/CartController.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using gellmvc.Domain.Abstract;
+using gellmvc.Domain.Entities;
+using gellmvc.Models;
+
+namespace gellmvc.Controllers
+{
+  public class CartController : CartBaseController
+  {
+    public CartController(IProductRepository repo) : base (repo) {}
+    
+    public RedirectToRouteResult RemoveFromCart(Cart cart, int Id, string returnUrl)
+    {
+      Product product = repository.Products.FirstOrDefault(p => p.Id == Id);
+
+      if (product != null)
+      {
+        cart.RemoveLine(product);
+      }
+      return RedirectToAction("Index", new { returnUrl });
+    }
+    
+    public RedirectToRouteResult ClearCart(Cart cart)
+    {
+      cart.Clear();
+      return RedirectToAction("Index");
+    }
+    
+    public ViewResult Index(Cart cart)
+    {
+      return View(LookUpProducts(cart));
+    }
+    
+    [HttpPut]
+    [Route("Cart/PutUpdate")]
+    public HttpStatusCodeResult PutUpdate(Cart cart, CartUpdate cartUpdate)
+    {
+      // Check if there are sufficient quantity in stock.
+      Product product = repository.Products.FirstOrDefault(p => p.Id == cartUpdate.ProductId);
+
+      if (product.QuantityInStock >= cartUpdate.NewQty)
+      {
+        // We have enough stock...
+        if (cartUpdate.NewQty == 0)
+        {
+          // User set qty to zero.
+          cart.RemoveLine(product);
+          cartResponseHeaders(cart, "updated-qty", 0, 0, "Updated cart - set qty to zero.");
+          return new HttpStatusCodeResult(System.Net.HttpStatusCode.OK);
+        }
+        else
+        {
+          if (cartUpdate.NewQty > 0)
+          {
+            // User set qty to positive integer. Update cart quantity.
+            Cart.CartLine line = cart.SetLineQuantity(product, cartUpdate.NewQty);
+            cartResponseHeaders(cart, "updated-qty", line.Quantity, line.SubTotal, "Updated cart.");
+            return new HttpStatusCodeResult(System.Net.HttpStatusCode.OK);
+          }
+          else
+          {
+            // User set qty to negative integer. Remove from cart.
+            cart.RemoveLine(product);
+            cartResponseHeaders(cart, "removed-from-cart", 0, 0, "Removed item from cart.");
+            return new HttpStatusCodeResult(System.Net.HttpStatusCode.OK);
+          }
+        }
+      }
+      else
+      {
+        // User wants more than we have available. Set to max available.
+        Cart.CartLine line = cart.SetLineQuantity(product, product.QuantityInStock);
+        Response.AddHeader("max", product.QuantityInStock.ToString());
+        cartResponseHeaders(cart,
+          "set-to-max",
+          product.QuantityInStock,
+          product.UnitPrice * product.QuantityInStock,
+          "Only " + product.QuantityInStock + " items available!"
+        );
+        return new HttpStatusCodeResult(System.Net.HttpStatusCode.OK);
+      }
+    }
+  }
+}
---
+++ IdentityApplication/Models/CartIndexViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using gellmvc.Domain.Entities;
+
+namespace gellmvc.Models
+{
+  public class CartIndexViewModel
+  {
+    public IEnumerable<ProductLine> ProductLines { get; set; }
+    public Cart Cart { get; set; }
+
+    public string User { get; set; }
+    public IEnumerable<UserAddress> Addresses { get; set; }
+
+    public int ShippingAddress { get; set; } // Id of the address object
+    public int BillingAddress { get; set; }  // Id of the address object
+  }
+
+  public class CartUpdate
+  {
+    public int ProductId { get; set; }
+    public int NewQty { get; set; }
+  }
+
+  public class UserAddress
+  {
+    public int Id { get; set; }
+    public string Value { get; set; }
+    public bool Deleted { get; set; }
+  }
+
+  public class AddressRowViewModel
+  {
+    public string User { get; set; }
+    public UserAddress UserAddress { get; set; }
+    public bool IsShipping { get; set; }
+    public bool IsBilling { get; set; }
+  }
+}
