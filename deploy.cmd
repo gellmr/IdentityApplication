460d75498d95507c8870a93246a21eb938061037
gellmr <gellmr@gmail.com>
2019-02-08 16:56:33 +0800

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -146,7 +146,7 @@ call :ExecuteCmd %bower install%
 IF !ERRORLEVEL! NEQ 0 goto error
 
 echo "running grunt..."
-grunt --verbose
+call :ExecuteCmd grunt
 echo "(finished running grunt)"
 IF !ERRORLEVEL! NEQ 0 goto error
 
